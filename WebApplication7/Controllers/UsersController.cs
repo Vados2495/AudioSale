f405c047eb24e84871513842a929b10e8e0acafa
Kova_qb51@student.itstep.org <Kova_qb51@student.itstep.org>
2023-12-05 19:18:47 +0200

    Функционал UsersController

--- WebApplication7/Controllers/UsersController.cs
+++ WebApplication7/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication7.Models;
 
 namespace WebApplication7.Controllers
 {
@@ -7,6 +9,74 @@ namespace WebApplication7.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
 
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/users
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        // GET: api/users/{id}
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUserById(int id)
+        {
+            var user = _context.Users.Find(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // POST: api/users
+        [HttpPost]
+        public ActionResult<User> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetUserById), new { id = user.UserID }, user);
+        }
+
+        // PUT: api/users/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, User user)
+        {
+            if (id != user.UserID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE: api/users/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = _context.Users.Find(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
