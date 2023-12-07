b712d2ddd05d590c9c4ef18a2cdef14c22550511
Vadim Kovalyov <vadoskovalyov06@gmail.com>
2023-12-07 18:35:48 +0200

    Обновил Product.cs

--- WebApplication7/Models/Product.cs
+++ WebApplication7/Models/Product.cs
@@ -8,11 +8,17 @@ namespace WebApplication7.Models
         public int ProductID { get; set; }
 
         [Required]
+        public byte[] Image { get; set; }
+
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         [Required]
         public int Price { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public Category Category { get; set; }
     }
 }
