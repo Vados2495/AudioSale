d7ac19f07a53c56d635f1e77103e206ebcda4595
Vadim Kovalyov <vadoskovalyov06@gmail.com>
2023-12-07 18:32:53 +0200

    Добавлено модель Category

---
+++ WebApplication7/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication7.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+
+        [Required]
+        public byte[] Image { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
