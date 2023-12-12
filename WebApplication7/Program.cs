3a000ca95b981b229c22516573b78e80f771f062
Vadim Kovalyov <vadoskovalyov06@gmail.com>
2023-12-12 19:02:03 +0200

    Updated

--- WebApplication7/Program.cs
+++ WebApplication7/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Configuration.AddJsonFile("appsettings.json");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
