985abbccf4656b43fd7e2fd0cb1da066086edd75
Vadim Kovalyov <vadoskovalyov06@gmail.com>
2023-12-12 18:26:47 +0200

    Добавил Startup.cs

---
+++ WebApplication7/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication7
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyOrigin", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors("AllowAnyOrigin");
+        }
+    }
+}
